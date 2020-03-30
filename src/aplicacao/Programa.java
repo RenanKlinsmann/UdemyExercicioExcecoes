a6eec7376d0684f38aa157c1ec19add7d07d10ef
Renan Klinsmann <klismann-nan@hotmail.com>
2020-03-30 17:20:26 -0300

    Projeto melhorado

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -17,7 +17,7 @@ public class Programa {
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-		System.out.print("Room number: ");
+		System.out.print("Numero do Quarto: ");
 		int number = sc.nextInt();
 		System.out.print("Check-in date (dd/MM/yyyy): ");
 		Date checkIn = sdf.parse(sc.next());
@@ -39,15 +39,12 @@ public class Programa {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
-			}
-			else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
-			}
-			else {
-				reservation.updateDates(checkIn, checkOut);
+			
+
+				String error = reservation.updateDates(checkIn, checkOut);
+				if(error != null) {
+					System.out.println("Error in reservation: "+ error);
+				}else {
 				System.out.println("Reservation: " + reservation);
 			}
 		}
